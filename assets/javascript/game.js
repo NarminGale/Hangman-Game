9e3179dbe608c1529a454d46afaa264386f73ea3
NarminGale <narmingale@gmail.com>
2021-04-14 16:44:05 +0400

    Completed functionality of whole game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,24 +15,31 @@ let students = [
 let word = document.querySelector('#word p')
 let guess = document.getElementById('guesses')
 let loss = document.getElementById('losses')
+let win = document.getElementById('wins')
 let guessedLetters = document.getElementById('guessedLetters')
 
 let userWord = []
 let guessedLetter = []
 
-let randomNumber = Math.floor(Math.random() * students.length)
-let student = students[randomNumber]
-
-for (let i = 0; i < student.length; i++) {
-  userWord.push('_')
-}
-word.innerHTML = userWord.join(' ')
-
 let guesses = 9
 let losses = 0
+let wins = 0
 
 guess.innerHTML = guesses
 loss.innerHTML = losses
+win.innerHTML = wins
+
+function selectRandomStudent() {
+  let randomNumber = Math.floor(Math.random() * students.length)
+  let student = students[randomNumber]
+
+  for (let i = 0; i < student.length; i++) {
+    userWord.push('_')
+  }
+  word.innerHTML = userWord.join(' ')
+  return student
+}
+let student = selectRandomStudent()
 
 window.onkeypress = function (e) {
   let userInput = e.key
@@ -48,9 +55,6 @@ window.onkeypress = function (e) {
     if (student.includes(userInput)) {
       let index = student.indexOf(userInput)
       userWord[index] = student[index]
-      // console.log(userWord)
-      // console.log(index)
-      // console.log(student[index])
       word.innerHTML = userWord.join(' ')
     } else {
       guesses -= 1
@@ -60,29 +64,45 @@ window.onkeypress = function (e) {
         losses += 1
         loss.innerHTML = losses
         guess.innerHTML = guesses
+
+        // to empty guessedLetters
+        guessedLetter = []
+        guessedLetters.innerHTML = guessedLetter
+
+        // new word
+        userWord = []
+        word.innerHTML = userWord.join('')
+
+        student = selectRandomStudent()
       }
     }
   } else if (a.length > 1) {
     for (let i = 0; i < a.length; i++) {
-      console.log(a[i])
       if (student.includes(userInput)) {
-        // let index = student.indexOf(userInput)
         userWord[a[i]] = student[a[i]]
-        // console.log(userWord)
-        // console.log(index)
-        // console.log(student[index])
         word.innerHTML = userWord.join(' ')
-      } else {
-        guesses -= 1
-        guess.innerHTML = guesses
-        if (guesses === 0) {
-          guesses = 9
-          losses += 1
-          loss.innerHTML = losses
-          guess.innerHTML = guesses
-        }
       }
     }
+  }
+
+  let joinUserWord = userWord.join('')
+  if (student === joinUserWord) {
+    // Added setTimeout for removing word and adding new word
+    const handle = setTimeout(() => {
+      userWord = []
+      word.innerHTML = userWord.join('')
+      student = selectRandomStudent()
+
+      guesses = 9
+      wins += 1
+      win.innerHTML = wins
+      guess.innerHTML = guesses
+
+      guessedLetter = []
+      guessedLetters.innerHTML = guessedLetter
+
+      clearTimeout(handle)
+    }, 700)
   }
 }
 
@@ -94,5 +114,4 @@ function checkIndex(userInput, student) {
     }
   }
   return indexArr
-  // console.log(indexArr.length)
 }
